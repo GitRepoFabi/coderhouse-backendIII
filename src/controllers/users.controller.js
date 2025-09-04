86ccf488c62b25eee3ce0966d23b827124f16cf7
Fabian Larrosa <fabianlarrosa2013@gmail.com>
2025-09-04 09:43:08 -0300

    Test completos y arreglos en archivos controllers agregando try/catch

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,32 +1,52 @@
 import { usersService } from "../services/index.js"
 
 const getAllUsers = async(req,res)=>{
-    const users = await usersService.getAll();
-    res.send({status:"success",payload:users})
+    try {
+        const users = await usersService.getAll();
+        res.send({status:"success",payload:users})        
+    } catch (error) {
+        res.status(500).send("Ha ocurrido un error en la petición, por favor ver detalle: ",error)
+    }
+    
 }
 
 const getUser = async(req,res)=> {
-    const userId = req.params.uid;
-    const user = await usersService.getUserById(userId);
-    if(!user) return res.status(404).send({status:"error",error:"User not found"})
-    res.send({status:"success",payload:user})
+
+    try {
+        const userId = req.params.uid;
+        const user = await usersService.getUserById(userId);
+        if(!user) return res.status(404).send({status:"error",error:"User not found"})
+        res.send({status:"success",payload:user})        
+    } catch (error) {
+        res.status(500).send("Ha ocurrido un error en la petición, por favor ver detalle: ",error)
+    }
 }
 
 const updateUser =async(req,res)=>{
-    const updateBody = req.body;
-    const userId = req.params.uid;
-    const user = await usersService.getUserById(userId);
-    if(!user) return res.status(404).send({status:"error", error:"User not found"})
-    const result = await usersService.update(userId,updateBody);
-    res.send({status:"success",message:"User updated"})
+
+    try {
+        const updateBody = req.body;
+        const userId = req.params.uid;
+        const user = await usersService.getUserById(userId);
+        if(!user) return res.status(404).send({status:"error", error:"User not found"})
+        const result = await usersService.update(userId,updateBody);
+        res.send({status:"success",message:"User updated"})        
+    } catch (error) {
+        res.status(500).send("Ha ocurrido un error en la petición, por favor ver detalle: ",error)
+    }
 }
 
 const deleteUser = async(req,res) =>{
-    const userId = req.params.uid;
-    const user = await usersService.getUserById(userId);
-    if(!user) return res.status(404).send({status:"error", error:"User not found"})
-    const result = await usersService.delete(user);
-    res.send({status:"success",message:"User deleted"})
+    try {
+        const userId = req.params.uid;
+        const user = await usersService.getUserById(userId);
+        if(!user) return res.status(404).send({status:"error", error:"User not found"})
+        const result = await usersService.delete(user);
+        res.send({status:"success",message:"User deleted"})        
+    } catch (error) {
+        res.status(500).send("Ha ocurrido un error en la petición, por favor ver detalle: ",error)
+    }
+
 }
 
 export default {
--- test/supertest.test.js
+++ test/supertest.test.js
@@ -15,6 +15,11 @@ describe("Pruebas router pets", function(){
         await mongoose.connection.collection("pets").deleteMany({specie:"test"})
     })
 
+    it("Si consulto todas las mascotas al endpoint /api/pets metodo GET, me debería devolver un array de mascotas", async() => {
+        let {body} = await requester.get("/api/pets").send()
+        expect(Array(body.payload))
+    })    
+
     it("Si envío los datos correctos de una mascota al /api/pets metodo POST, da de alta la mascota en DB", async()=>{
         let petMock={
             name: "Rocky", 
@@ -59,5 +64,46 @@ describe("Pruebas router pets", function(){
 
         let {status}=await requester.post("/api/pets").send(petMock)
         expect(status).to.be.eq(400)
-    })            
+    })        
+    
+})
+
+describe("Pruebas router users", function(){
+    this.timeout(10_000) // 2.000 ms
+
+    it("Si consulto todos los usuarios al endpoint /api/users metodo GET, me debería devolver un array de usuarios", async() => {
+        let {body} = await requester.get("/api/users").send()
+        expect(Array(body.payload))
+    })        
+
+    it("Si quiero generar 1 usuario y 1 mascota, al apuntar al endpoint /api/mocks/generateData metodo POST sin pasarle cantidad, da de alta 1 usuario y 1 mascota en DB", async() => {
+
+        let {status, body}=await requester.post(`/api/mocks/generateData`).send()
+        expect(status).to.be.eq(201)
+        expect(Array(body))
+        expect(body.message).to.be.eq("Datos generados exitosamente")
+    })
+
+    it("Si quiero generar N usuarios y N mascotas, al apuntar al endpoint /api/mocks/generateData metodo POST pasándole la cantidad deseada, da de alta los N usuarios y las N mascotas en DB", async() => {
+
+        let usuarios=3
+        let mascotas=3
+
+        let {status, body}=await requester.post(`/api/mocks/generateData/?user=${usuarios}&pets=${mascotas}`).send()
+        expect(status).to.be.eq(201)
+        expect(Array(body))
+        expect(body.message).to.be.eq("Datos generados exitosamente")
+    })    
+    
+    it("Si al apuntar al endpoint /api/mocks/generateData metodo POST le paso una cantidad negativa en el parámetro para generar un usuario, debe de dar error", async() => {
+        let user = -1
+        let {status}=await requester.post(`/api/mocks/generateData/?user=${user}`).send()
+        expect(status).to.be.eq(400)
+    })    
+
+    it("Si al apuntar al endpoint /api/mocks/generateData metodo POST le paso una cantidad negativa en el parámetro para generar una mascota, debe de dar error", async() => {
+        let pet = -1
+        let {status}=await requester.post(`/api/mocks/generateData/?pet=${pet}`).send()
+        expect(status).to.be.eq(400)
+    })
 })
