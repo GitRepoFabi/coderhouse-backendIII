b9d943e6997579745670cd7846dec7b3cad6d67e
Fabian Larrosa <fabianlarrosa2013@gmail.com>
2025-09-03 21:14:00 -0300

    Comienzo de testing unitario

---
+++ test/supertest.test.js
@@ -0,0 +1,63 @@
+import { describe, it } from "mocha";
+import supertest from "supertest";
+import { expect } from "chai";
+import config from "../src/config/config.js";
+import mongoose, { isValidObjectId } from "mongoose"
+
+const requester=supertest(`http://localhost:${config.PORT}`)
+
+await mongoose.connect(config.MONGO_URL,{dbName:config.DB_NAME})
+
+describe("Pruebas router pets", function(){
+    this.timeout(10_000) // 2.000 ms
+
+    after(async()=>{
+        await mongoose.connection.collection("pets").deleteMany({specie:"test"})
+    })
+
+    it("Si envío los datos correctos de una mascota al /api/pets metodo POST, da de alta la mascota en DB", async()=>{
+        let petMock={
+            name: "Rocky", 
+            specie: "test", 
+            birthDate: new Date(2025, 11, 18).toUTCString()
+        }
+
+        let {status, body}=await requester.post("/api/pets").send(petMock)
+        expect(status).to.be.eq(200)
+        expect(body.payload).to.has.property("_id")
+        expect(isValidObjectId(body.payload._id)).to.be.true
+    })
+
+    it("Si envío los datos de la mascota sin el campo name al /api/pets metodo POST, me debería dar un error", async()=>{
+        let petMock={
+            //name: "Rocky", 
+            specie: "test", 
+            birthDate: new Date(2025, 11, 18).toUTCString()
+        }
+
+        let {status}=await requester.post("/api/pets").send(petMock)
+        expect(status).to.be.eq(400)
+    })    
+
+    it("Si envío los datos de la mascota sin el campo specie al /api/pets metodo POST, me debería dar un error", async()=>{
+        let petMock={
+            name: "Rocky", 
+            //specie: "test", 
+            birthDate: new Date(2025, 11, 18).toUTCString()
+        }
+
+        let {status}=await requester.post("/api/pets").send(petMock)
+        expect(status).to.be.eq(400)
+    })        
+    
+    it("Si envío los datos de la mascota sin el campo birthDate al /api/pets metodo POST, me debería dar un error", async()=>{
+        let petMock={
+            name: "Rocky", 
+            specie: "test", 
+            //birthDate: new Date(2025, 11, 18).toUTCString()
+        }
+
+        let {status}=await requester.post("/api/pets").send(petMock)
+        expect(status).to.be.eq(400)
+    })            
+})
